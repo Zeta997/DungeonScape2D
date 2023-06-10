f3e31f0ad0e94510acdf1edb8124285afd0bc9d3
José Javier <70950479+Zeta997@users.noreply.github.com>
2023-06-10 15:49:32 +0200

    configuration enemy

--- DungeonScape/Assets/Scripts/Enemy/Enemy.cs
+++ DungeonScape/Assets/Scripts/Enemy/Enemy.cs
@@ -9,20 +9,54 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected int health;
     [SerializeField] protected int speed;
     [SerializeField] protected int gems;
-    //[SerializeField] protected bool switchpoint;
+    
     #endregion
 
     #region Components
     [Header("Componentes")]
     [SerializeField] protected Transform _pointA, _pointB;
+    protected Vector2 _currentTarget;
+    protected SpriteRenderer _spriteEnemy;
+    protected Animator _enemyAN;
     #endregion
-
+    public virtual void Start()
+    {
+        _spriteEnemy = GetComponentInChildren<SpriteRenderer>();
+        _enemyAN = GetComponentInChildren<Animator>();
+    }
     public virtual void Attack()
     {
         //base attack
     }
 
-    public abstract void Update();
+    public virtual void Update()
+    {
+        if (_enemyAN.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        MovementEnemy();
+    }
 
-    public abstract void Start();
+    
+
+    public virtual void MovementEnemy()
+    {
+        if (transform.position == _pointA.position)
+        {
+
+            _spriteEnemy.flipX = false;
+            _currentTarget = _pointB.position;
+            _enemyAN.SetTrigger("Idle");
+        }
+        else if (transform.position == _pointB.position)
+        {
+
+            _spriteEnemy.flipX = true;
+            _currentTarget = _pointA.position;
+            _enemyAN.SetTrigger("Idle");
+        }
+        transform.position = Vector2.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+
+    }
 }
--- DungeonScape/Assets/Scripts/Enemy/MossGiantScript.cs
+++ DungeonScape/Assets/Scripts/Enemy/MossGiantScript.cs
@@ -4,51 +4,8 @@ using UnityEngine;
 
 public class MossGiantScript : Enemy
 {
-    #region components
-    private Animator _EnemyIdle;
-    private Vector2 _currenttarget;
-    private SpriteRenderer _enemyMossGianFlip;
-    #endregion
     public override void Start()
     {
-        _EnemyIdle = GetComponentInChildren<Animator>();
-        _enemyMossGianFlip = GetComponentInChildren<SpriteRenderer>();
+        base.Start();
     }
-    public override void Update()
-    {
-        //CheckingAnimations();
-        if (_EnemyIdle.GetCurrentAnimatorStateInfo(0).IsName("MossGiantIdle"))
-        {
-            return;
-        }
-        MovementEnemy();
-    }
-
-    public override void Attack()
-    {
-        base.Attack();
-
-        //avanced attack
-    }
-
-    void MovementEnemy()
-    {
-        if (transform.position== _pointA.position)
-        {
-            
-            _enemyMossGianFlip.flipX = false;
-            _currenttarget = _pointB.position;
-            _EnemyIdle.SetTrigger("Idle");
-        }
-        else if (transform.position == _pointB.position)
-        {
-            
-            _enemyMossGianFlip.flipX = true;
-            _currenttarget = _pointA.position;
-            _EnemyIdle.SetTrigger("Idle");
-        } 
-        transform.position = Vector2.MoveTowards(transform.position, _currenttarget, speed*Time.deltaTime);
-        
-    }
-
 }
